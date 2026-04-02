464d8c2bfd6d09c5ebb5bb9bcf02efe17db51f17
Pbi-with-love <anhtai210406@gmail.com>
2026-04-02 21:52:58 +0300

    fix sth

---
+++ frontend/src/api/Auth.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:3000/api/auth";
+
+export const login = async (loginId, password) => {
+    try {
+
+    } catch (error) {
+        console.error(" ", error);
+        throw error;
+    }
+}
+
+export const logout = async () => {
+    try {
+
+    } catch (error) {
+        console.error(" ", error);
+        throw error;
+    }
+}
